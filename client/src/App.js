d10352b836219978716a580a8467bf4d5f5cfc49
SaranyaRaj24 <saranyaraj243@gmail.com>
2024-09-23 15:37:45 +0530

    pp

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Navbar from "./Components/Navbar/Navbar";
+import Assigned from "./Components/Assigned/Assigned";
+import Reports from "./Components/Reports/Reports";
+import Priority from "./Components/Priority/Priority";
+import Pending from "./Components/Pending/Pending";
+import Notification from "./Components/Notification/Notification";
+import Settings from "./Components/Settings/Settings";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React ll2222
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route index element={<Navigate to="/dashboard" />} />
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/Navbar" element={<Navbar />} />
+        <Route path="/Assigned" element={<Assigned />} />
+        <Route path="/Reports" element={<Reports />} />
+        <Route path="/Priority" element={<Priority />} />
+        <Route path="/Pending" element={<Pending />} />
+        <Route path="/Notification" element={<Notification />} />
+        <Route path="/Settings" element={<Settings />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/Components/Navbar/Navbar.js
@@ -0,0 +1,229 @@
+
+import React from "react";
+import { FaUserCircle, FaBars } from "react-icons/fa";
+import "./Navbar.css";
+import checklistImage from "../../Assets/checklist.jpg.jpg";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/Inbox";
+import MailIcon from "@mui/icons-material/Mail";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { MdAssignmentTurnedIn, MdAssignment } from "react-icons/md";
+import { BiSolidHourglassBottom } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
+import Image from "../../Assets/task.jpg";
+
+function Navbar() {
+  const navigate = useNavigate();
+  const [state, setState] = React.useState({
+    left: false,
+  });
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [activeView, setActiveView] = React.useState("");
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setState({ ...state, left: open });
+  };
+
+  const handleCardClick = (path) => {
+    navigate(path);
+  };
+
+  const handleProfileClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    console.log("Logged out");
+    handleCloseMenu();
+  };
+
+  const handleSidebarClick = (path) => {
+    switch (path) {
+      case "/daily-checklist":
+        path = "/assigned";
+        break;
+      case "/pending-checklist":
+        path = "/pending";
+        break;
+      default:
+        break;
+    }
+    navigate(path);
+    setActiveView(path === "/dashboard" ? "dashboard" : "");
+  };
+
+  const list = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {["Dashboard", "Daily Checklist", "Pending checklist", "Priority"].map(
+          (text, index) => {
+            const path = `/${text.replace(" ", "-").toLowerCase()}`;
+            return (
+              <ListItem key={text} disablePadding>
+                <ListItemButton onClick={() => handleSidebarClick(path)}>
+                  <ListItemIcon style={{ color: "#25274D" }}>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            );
+          }
+        )}
+      </List>
+      <Divider />
+      <List>
+        {["Performance Reports", "Settings", "Notification"].map(
+          (text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton
+                onClick={() => {
+                  if (text === "Performance Reports") {
+                    navigate("/reports");
+                  } else if (text === "Notifications") {
+                    navigate("/notification"); 
+                  }
+                }}
+              >
+                <ListItemIcon style={{ color: "#25274D" }}>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          )
+        )}
+      </List>
+    </Box>
+  );
+
+  const renderDashboardContent = () => (
+    <div className="dashboard-cards">
+      <Card className="welcome-card">
+        <CardContent>
+          <Typography
+            style={{ fontSize: "2rem" }}
+            variant="h6"
+            component="div"
+            className="card-title"
+          >
+            Hi User
+            Welcome to Checklist-Genie!
+            <img
+              src={Image}
+              alt="task"
+              style={{ width: "14rem", height: "9rem", marginLeft: "25rem" }}
+            />
+          </Typography>
+          <Typography
+            variant="body1"
+            component="div"
+            className="card-description"
+          >
+            Check out any upcoming checklist and recent project below!
+          </Typography>
+        </CardContent>
+      </Card>
+      <div className="task-cards">
+        <Card
+          onClick={() => handleCardClick("/assigned")}
+          className="clickable-card"
+        >
+          <CardContent>
+            <Typography variant="h5" component="div" className="card-title">
+              Assigned checklist <MdAssignment className="card-icon" />
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card
+          onClick={() => handleCardClick("/pending")}
+          className="clickable-card"
+        >
+          <CardContent>
+            <Typography variant="h5" component="div" className="card-title">
+              Pending checklist <BiSolidHourglassBottom className="card-icon" />
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card
+          onClick={() => handleCardClick("/completed-tasks")}
+          className="clickable-card"
+        >
+          <CardContent>
+            <Typography variant="h5" component="div" className="card-title">
+              Completed checklist <MdAssignmentTurnedIn className="card-icon" />
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+
+  return (
+    <>
+      <div>
+        <header className="container">
+          <Button onClick={toggleDrawer(true)} className="drawer-button">
+            <FaBars />
+          </Button>
+          <div className="check">
+            <img src={checklistImage} alt="Checklist" />
+          </div>
+          <div className="pro" onClick={handleProfileClick}>
+            <FaUserCircle />
+          </div>
+        </header>
+
+        <main className="main-content">
+          {activeView === "dashboard" && renderDashboardContent()}
+        </main>
+
+        <Drawer anchor="left" open={state.left} onClose={toggleDrawer(false)}>
+          {list()}
+        </Drawer>
+
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleCloseMenu}
+          PaperProps={{
+            style: {
+              width: "200px",
+            },
+          }}
+        >
+          <MenuItem onClick={handleLogout}>Logout</MenuItem>
+        </Menu>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
